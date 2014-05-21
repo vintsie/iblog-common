10e58a29167673afc920592d39beaf78b4400dee
Vin Tsie <vin.tsie@hotmail.com>
2014-05-21 09:58:32 +0800

    Yes, 1st edition.

---
+++ src/main/java/org/vint/iblog/common/constant/GlobalVerbs.java
@@ -0,0 +1,10 @@
+package org.vint.iblog.common.constant;
+
+/**
+ * 全局静态数据
+ * Created by Vin on 14-5-20.
+ */
+public class GlobalVerbs {
+
+    public final static String DEFAULT_CATALOG = "programming";
+}
